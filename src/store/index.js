657455dbc5b06c2075180258f1d39e913335aa14
Adesanya Ayokunle <adesanyaayokunle@gmail.com>
2020-04-06 15:33:50 +0100

    update develop (#11)

--- src/store/index.js
+++ src/store/index.js
@@ -12,9 +12,8 @@ const store = createStore(
     initialState,
     compose(
         applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : compose
     )
 );
-
 sagaMiddleware.run(rootSaga);
 export default store;
