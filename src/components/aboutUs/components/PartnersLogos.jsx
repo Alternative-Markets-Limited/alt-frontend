11e4091497353b66e612e14087e97fe704c938cd
Adesanya Ayokunle <adesanyaayokunle@gmail.com>
2020-04-17 14:54:14 +0100

    completed about us page

---
+++ src/components/aboutUs/components/PartnersLogos.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import gpp from '../../../assets/images/gpp.png';
+import gppHover from '../../../assets/images/gpp-hover.png';
+import zenith from '../../../assets/images/zenith.png';
+import zenithHover from '../../../assets/images/zenith-hover.png';
+import creativeEnterprise from '../../../assets/images/creative-enterprise.png';
+import creativeEnterpriseHover from '../../../assets/images/creative-enterprise-hover.png';
+import flutterwave from '../../../assets/images/flutterwave.png';
+import flutterwaveHover from '../../../assets/images/flutterwave-hover.png';
+import verified from '../../../assets/images/verified.png';
+import verifiedHover from '../../../assets/images/verified-hover.png';
+import nedcom from '../../../assets/images/nedcom.png';
+import nedcomHover from '../../../assets/images/nedcom-hover.png';
+
+import { Logos } from './Logos';
+
+const logos = [
+    {
+        alt: 'gpp', img: gpp, imgHover: gppHover, key: 1,
+    },
+    {
+        alt: 'zenit', img: zenith, imgHover: zenithHover, key: 2,
+    },
+    {
+        alt: 'creative enterprise', img: creativeEnterprise, imgHover: creativeEnterpriseHover, key: 3,
+    },
+    {
+        alt: 'flutterwave', img: flutterwave, imgHover: flutterwaveHover, key: 4,
+    },
+    {
+        alt: 'verified', img: verified, imgHover: verifiedHover, key: 5,
+    },
+    {
+        alt: 'nedcom', img: nedcom, imgHover: nedcomHover, key: 6,
+    },
+];
+
+export const PartnersLogos = () => (
+    <div className="flex flex-row overflow-x-auto scrolling-touch items-center justify-between">
+        {logos.map(({
+            alt, img, key, imgHover,
+        }) => (
+            <Logos key={key} alt={alt} img={img} imgHover={imgHover} />
+        ))}
+    </div>
+);
--- src/components/layouts/components/SubHeader.jsx
+++ src/components/layouts/components/SubHeader.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const SubHeader = ({ name, className }) => (
+export const SubHeader = ({ name, className, textColor }) => (
     <div className={`${className} container text-xl md:text-2xl lg:text-3xl font-bold mb-8 flex flex-col items-center text-center `}>
-        <h3 className="my-1">{name}</h3>
+        <h3 className={`my-1 ${textColor}`}>{name}</h3>
         <div className="border border-alt-green w-12" />
     </div>
 );
@@ -11,9 +11,11 @@ export const SubHeader = ({ name, className }) => (
 SubHeader.propTypes = {
     className: PropTypes.string,
     name: PropTypes.string.isRequired,
+    textColor: PropTypes.string,
 };
 
 SubHeader.defaultProps = {
     className: '',
+    textColor: 'text-black',
 };
 
