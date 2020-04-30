9691024ad3a1c992ffa3979b2898cfcff59a44f6
Adesanya Ayokunle <adesanyaayokunle@gmail.com>
2020-04-30 16:20:30 +0100

    added search functionality and fixed bugs (#23)

--- src/components/common/helpers.js
+++ src/components/common/helpers.js
@@ -6,3 +6,20 @@
 
 export const formatMoney = amount => amount.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
 
+/**
+ * Filters an array of objects by custom predicates.
+ *
+ * @param  {Array}  array: the array to filter
+ * @param  {Object} filters: an object with the filter criteria
+ * @return {Array}
+ */
+export const filterArray = (array, filters) => {
+    const filterKeys = Object.keys(filters);
+    // validates all filter criteria
+    return array.filter(item => filterKeys.every(key => {
+        if (typeof filters[key] !== 'function') return true;
+        // ignores empty filters
+        if (!filters[key].length) return true;
+        return filters[key](item[key]);
+    }));
+};
