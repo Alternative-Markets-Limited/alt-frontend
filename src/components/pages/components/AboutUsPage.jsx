12cf3c68fadcc4eb6033761056bac871fe65e35f
Enilolobo Eniola <eniloloboeniolaisrael@gmail.com>
2020-05-01 01:49:26 +0100

    changed the favicon

--- src/components/pages/components/AboutUsPage.jsx
+++ src/components/pages/components/AboutUsPage.jsx
@@ -1,4 +1,16 @@
 import React from 'react';
 import { AboutUs } from '../../aboutUs/components';
-
-export const AboutUsPage = () => <AboutUs />;
+import { Helmet } from "react-helmet";
+ 
+export const AboutUsPage = () => 
+{
+	return(
+			<div>
+				<Helmet>
+					<title>{ "About Us" }</title>
+				</Helmet>
+				<AboutUs />
+			</div>
+		);
+ }
+// <AboutUs />;
