5e2ca0e9526e1423632f2e072e45fb7469ab0685
Enilolobo Eniola Israel <62079069+Enilolobo-Eniola@users.noreply.github.com>
2020-05-02 18:30:13 +0100

    made the page titles dynamic and changes the favicon (#26)

--- src/components/pages/components/AboutUsPage.jsx
+++ src/components/pages/components/AboutUsPage.jsx
@@ -1,4 +1,15 @@
 import React from 'react';
 import { AboutUs } from '../../aboutUs/components';
+import { Helmet } from "react-helmet";
 
-export const AboutUsPage = () => <AboutUs />;
+export const AboutUsPage = () => 
+{
+	return(
+		<div>
+			<Helmet>
+				<title>{"About Us"}</title>
+			</Helmet>
+			<AboutUs />
+		</div>
+	);
+}
