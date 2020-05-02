dd9ceca12dc15c7a7d722dfd42443d9e4c06b2d0
Adesanya Ayokunle <adesanyaayokunle@gmail.com>
2020-05-02 18:42:40 +0100

    fixed lint issues

--- src/components/pages/components/LoginPage.jsx
+++ src/components/pages/components/LoginPage.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { Login } from '../../login/components';
-import { Helmet } from "react-helmet";
 
-export const LoginPage = () => 
-{
-	return(
-		<div>
-			<Helmet>
-				<title>{"Login"}</title>
-			</Helmet>
-			<Login />
-		</div>
-	);
-}
+export const LoginPage = () => (
+    <div>
+        <Helmet>
+            <title>Login</title>
+        </Helmet>
+        <Login />
+    </div>
+);
