daa5e860a2310f197a7806feacc38ff7ca3f8fb9
Enilolobo Eniola <eniloloboeniolaisrael@gmail.com>
2020-05-02 01:00:46 +0100

    made the page titles dynamic and changes the favicon

--- src/components/pages/components/HomePage.jsx
+++ src/components/pages/components/HomePage.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 import { Home } from '../../home/components';
+import { Helmet } from "react-helmet";
 
-export const HomePage = () => <Home />;
+export const HomePage = () =>
+{
+	return(
+		<div>
+			<Helmet>
+				<title>{"Alt | Home"}</title>
+			</Helmet>
+			<Home />
+		</div>
+	);
+}
 
--- src/components/pages/components/LoginPage.jsx
+++ src/components/pages/components/LoginPage.jsx
@@ -1,4 +1,15 @@
 import React from 'react';
 import { Login } from '../../login/components';
+import { Helmet } from "react-helmet";
 
-export const LoginPage = () => <Login />;
+export const LoginPage = () => 
+{
+	return(
+		<div>
+			<Helmet>
+				<title>{"Login"}</title>
+			</Helmet>
+			<Login />
+		</div>
+	);
+}
